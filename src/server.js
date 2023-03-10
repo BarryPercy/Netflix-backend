f272140c300837d18cdd4c2b96410e7df5675a16
Barry Percy <baz.percy@gmail.com>
2023-03-10 16:02:59 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ server.use(Express.static(publicFolderPath))
 
 const whiteList = [process.env.FE_DEV_URL, process.env.FE_PROD_URL]
 
-console.log(whiteList)
+console.log("whitelist->",whiteList)
 const corsOptions = {
   origin: (origin,corsNext) => {
     if(!origin || whiteList.indexOf(origin)!==-1){
